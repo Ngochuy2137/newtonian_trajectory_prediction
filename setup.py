9823d4dbef45f7987c2ffbea6fee8b26b0de23ab
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-06-17 07:29:54 +0900

    change package name in setup.py -> newtonian_trajectory_prediction
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='newtonian_law_trajectory_prediction',
+    name='newtonian_trajectory_prediction',
     version='0.1',
     packages=find_packages(),
     install_requires=[],
